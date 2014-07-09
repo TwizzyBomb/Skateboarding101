e437477fa2a23450ddfe6ee229d3a4839b793057
User <User@Brian-PC>
2014-07-09 14:19:09 -0400

    Functionally Working Reflex Game 0.3 Timer Working, Score working Work on:  Add keyboard Input  Fancy Start Page  Measuring Reflex time!  Scoring system - Needs to reset after fails, and To be defined
    Working on the animation which will start and stop games - getting Play span to work. Seems to work until animation completes.
    
     Method to have multiple games on the site (without taking too much resources)
    
    Started Coding Tutorials, Created Useful Snippets JS file

--- web/js/codeSnippetsFromLife.js
+++ web/js/codeSnippetsFromLife.js
@@ -101,3 +101,64 @@ function FirstReverse(str) {
   // code goes here  
   return theString;
 }
+    
+function LetterChanges(str) { 
+  //figure out how to not do this to the * reg expression
+  //var str2;
+  var vowels = ["a", "e", "i", "o", "u"];
+  var len = str.length;
+  var tmp = ""
+  var charCode;
+  var newString = "";
+  //reg exp - all letters
+  var regEx = /[A-Za-z]/;
+  for(var i=0;i<=len;i++){
+      
+    tmp = str.charAt(i);
+    
+    //tmp = a
+    console.log("tmp = " + tmp);
+    
+    charCode = tmp.charCodeAt();
+    //charCode should equal 67, the code of a
+    console.log("charCode = " + charCode);
+    
+    charCode+=1;
+    //charCode should equal 68 and be capitalized if vowel
+    newLetter = String.fromCharCode(charCode);
+    //checks if new string contains vowel
+    if(vowels.indexOf(newLetter) > -1){
+    switch(newLetter){
+    case "a":
+        newLetter = "A";
+        break;
+    case "e":
+        newLetter = "E";
+        break;
+    case "i":
+        newLetter = "I";
+        break;
+    case "o":
+        newLetter = "O";
+        break;
+    case "u":
+        newLetter = "U";
+        break;
+    }
+        console.log("print this vowel " + newLetter);
+        
+    }
+    console.log("new charCode = " + charCode);
+    
+
+    //checks if newLetter is a letter
+    if(regEx.test(tmp)===true ){
+        newString+=newLetter;
+    }else{
+       newString+=tmp;
+    }
+    //newString adds b to string
+    console.log("newString = " + newString); 
+  }
+  return newString; 
+}
